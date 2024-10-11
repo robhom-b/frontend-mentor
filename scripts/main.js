8d0d9035b698f04c5b14629d2698e2d722595654
robhom-b <rob.hom@bixal.com>
2024-10-11 15:53:10 -0400

    Link JS file to html and add button
    Connected the project JS file to the home page and added a button. The button is only set up for an alert but ideally for a color switcher.

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function color(){
+    //Get the button
+    alert("The button is clicked");
+    //Define the styles to toggle
+
+    //Initial state
+
+    //Function to toggle style
+
+    //Add event listener to button
+}

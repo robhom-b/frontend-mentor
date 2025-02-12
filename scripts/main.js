47a145a62978b92cc690ea328e07b014fa342271
robhom-b <rob.hom@bixal.com>
2025-02-12 15:57:28 -0500

    Clean up color palette
    Added in hover states, replaced IG profile with BSky, explore how to keep name header color consistent with light or dark theme

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,5 @@
-/*
-Click the button! I'm writing a script to change the social media card's color palette.
-The new style will toggle on or off a new theme after the user clicks the Style Me button.
-
-    1. Get the button - done
-    2. Define the styles to toggle - done
-    3. Function to toggle style
-    4. Add event listener to button - done
-*/
+/*Click the button! I'm writing a script to change the social media card's color palette.
+The new style will toggle on or off a new theme after the user clicks the Style Me button.*/
 
 //Creates a new function called switchStyle that takes a parameter value
   function switchStyle(buttonGroup){
@@ -15,14 +8,17 @@ The new style will toggle on or off a new theme after the user clicks the Style 
     //console.log(element); //Use log to check the value of the element variable
     socialButtons.forEach(button => button.classList.toggle("social-button-active-state")) //iterate over the socialButtons objects to toggle on or off the social-button-active-state class
 
+    const taglines = document.querySelectorAll(".tagline");
+    taglines.forEach(tagline => tagline.classList.toggle("active"));
+
     const taglineText = document.querySelector(".tagline-default-state"); //the taglineText variable is assigned a collection of elements with the .tagline-state class
-    console.log(taglineText.classList); //Use log to check the value of the taglineText variable
-    taglineText.classList.toggle("tagline-active-state"); //Use the toggle method to remove the given argument from the DOMTokenList
+    //console.log(taglineText.classList); //Use log to check the value of the taglineText variable
+    /* taglineText.classList.toggle("tagline-active-state"); //Use the toggle method to remove the given argument from the DOMTokenList
     console.log(taglineText.classList);
     //how do I add the tagline-state class back into the DOMTokenList after the button has been clicked?
 
     const aboutText = document.querySelector(".about-text-default-state");
-    aboutText.classList.toggle("about-text-active-state");
+    aboutText.classList.toggle("about-text-active-state"); */
 
     const cardBackground = document.querySelector(".inner-container");
     cardBackground.classList.toggle("cardBackground-active-state");
@@ -40,31 +36,7 @@ The new style will toggle on or off a new theme after the user clicks the Style 
     Because instead of haivng the default state class in the querySelector method, I could just dive
     straight into with the original element and CSS selector in the query selector.
 
+    It's better to have as little code as possible to save on space and dollars.
+
     Having the default and active states though did help me figure it out quicker!
     */
-
-
-
-  /*for(let i=0; i < 3, i++)
-    {
-    //const element = document.querySelector('div#socialButton.social-container.titillium-web-semibold a'); 
-    // //gets the first element with the class social container
-    element.forstyle = "background-color: red";
-    };*/
-
-    
-  /*function switchStyle(elementId, styleName) {
-    const element = document.querySelector('div#socialButton.social-container.titillium-web-semibold a'); //gets the first element with the class social container
-    element.style = "background-color: red";
-    
-    console.log(element.style);
-    console.log("The value of element is " + element);
-    console.log("The function works");
-  }
-  
-  const styles = {
-    style1: "darkMode",
-    style2: "lightMode"
-  };
-  
-  */

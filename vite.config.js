f3d8670a01a39f967f01910eefc06e7405b20354
robhom-b <rob.hom@bixal.com>
2025-07-23 15:03:09 -0400

    Add Form to Dist

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        form: resolve(__dirname, 'form.html')
+      }
+    }
   },
   server: {
     port: 3000
